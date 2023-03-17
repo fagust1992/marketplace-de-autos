06692b26663789629a54ca1fbfb47c26458a41d8
francisco august <fraciscoaugust19921992@gmail.com>
2023-03-17 15:25:34 -0600

    subiendo pagina de detalla producto part1

--- src/Paginas/Publicas/DetalleProduct.jsx
+++ src/Paginas/Publicas/DetalleProduct.jsx
@@ -1,16 +1,54 @@
 import { Context } from "../../Context/Context";
 import { useContext } from "react";
 import { useParams } from "react-router-dom";
+import Footer from "../../layouts/Footer";
 
 export function Detalleproducto() {
   const { id } = useParams();
   const { dato } = useContext(Context);
   const productos = dato.products;
-  const x = productos?.findIndex((x) => x.id === parseInt(id));
-  console.log(x);
+
+  const x = productos?.findIndex((x) => x.id === Number(id));
+
   return (
     <>
-      <h1>soy detalle product</h1>
+      {productos ? (
+        <div className="padre  d-flex row">
+          <div className="col  " style={{ color: "#fff" }}>
+            <div className="imagen-carta">
+              <img
+                src={productos[x].productUrl}
+                alt=""
+                className="imagen-detalle"
+              />
+            </div>
+          </div>
+          <div
+            className="col  texto-detalle-product "
+            style={{ color: "black" }}
+          >
+            <div className="imagen-texto">
+              <p>
+                <label className="nombre">Nombre:</label>
+                {productos[x].nombre}
+              </p>
+              <label>Marca:</label>
+              <p>{productos[x].marca}</p>
+              <label>Kilometros:</label>
+              <p>{productos[x].kilometros}</p>
+              <label>Propietario:</label>
+              <p>{productos[x].propietario}</p>
+              <label>Tipo de Combustible:</label>
+              <p>{productos[x].tipo_de_combustible}</p>
+              <label>Precio:</label>
+              <p>{productos[x].precio}</p>
+              <button className="boton-detalle-producto">Contactar</button>
+            </div>
+          </div>
+        </div>
+      ) : null}
+
+      <Footer />
     </>
   );
 }

c599c51ac1c92c6624dfbf1f8a5e2f3377acb68a
francisco august <fraciscoaugust19921992@gmail.com>
2023-04-03 22:34:34 -0600

    otro

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -32,7 +32,7 @@ export default function Navbar() {
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/formulario">
+              <Link className="nav-link" to="/login">
                 LogIn
                 <a>
                   {" "}
